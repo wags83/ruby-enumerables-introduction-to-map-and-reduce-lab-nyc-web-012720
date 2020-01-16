b5b687ff75023590f7e613f6b95af0e38bf7decb
Greg Wagner <gpwagner@gmail.com>
2020-01-16 14:03:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,33 @@ def map_to_negativize(array)
   end
   return_array
 end
+
+def map_to_no_change(array)
+  i=0
+  return_array = []
+  while i < array.length do
+    return_array[i] = array[i]
+    i += 1
+  end
+  return_array
+end
+
+def map_to_negativize(array)
+  i=0
+  return_array = []
+  while i < array.length do
+    return_array[i] = -1*array[i]
+    i += 1
+  end
+  return_array
+end
+
+def map_to_negativize(array)
+  i=0
+  return_array = []
+  while i < array.length do
+    return_array[i] = -1*array[i]
+    i += 1
+  end
+  return_array
+end
